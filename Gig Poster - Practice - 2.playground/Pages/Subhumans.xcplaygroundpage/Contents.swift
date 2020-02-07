1025a992dcb81323f5eda0bedb81f6a4a17a943e
Scott Doggett <sdoggett@lcs.on.ca>
2020-02-07 11:29:33 -0500

    done everything except some words

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -34,6 +34,86 @@ let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
 
 // Begin your solution here...
 
+canvas.drawShapesWithBorders = false
+
+canvas.fillColor = yellow
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: canvas.width, height: canvas.height)
+
+for x in stride(from: -135, through: 345, by: 150 + 10) {
+    
+    // Change colour based on position
+    if x == 135 || x == 185 {
+        canvas.fillColor = brown
+    } else {
+        canvas.fillColor = purple
+    }
+    
+    var polygonVertices: [Point] = []
+    polygonVertices.append(Point(x: x, y: 50))
+    polygonVertices.append(Point(x: x + 150, y: 50))
+    polygonVertices.append(Point(x: x + 200, y: 150))
+    polygonVertices.append(Point(x: x + 50, y: 150))
+
+    canvas.drawCustomShape(with: polygonVertices)
+    
+}
+
+for x in stride(from: -135, through: 345, by: 150 + 10) {
+    
+    // Change colour based on postion
+    if x == 135 || x == 185 {
+        canvas.fillColor = brown
+    } else {
+        canvas.fillColor = purple
+    }
+    
+    var polygonVertices: [Point] = []
+    polygonVertices.append(Point(x: x, y: 225))
+    polygonVertices.append(Point(x: x + 150, y: 225))
+    polygonVertices.append(Point(x: x + 200, y: 325))
+    polygonVertices.append(Point(x: x + 50, y: 325))
+
+    canvas.drawCustomShape(with: polygonVertices)
+    
+}
+
+for x in stride(from: -135, through: 345, by: 150 + 10) {
+    
+    // Set colour to red
+    canvas.fillColor = lightRed
+    
+    var polygonVertices: [Point] = []
+    polygonVertices.append(Point(x: x, y: 140))
+    polygonVertices.append(Point(x: x + 150, y: 140))
+    polygonVertices.append(Point(x: x + 200, y: 240))
+    polygonVertices.append(Point(x: x + 50, y: 240))
+
+    canvas.drawCustomShape(with: polygonVertices)
+
+}
+
+canvas.drawText(message: "subhumans", at: Point(x: 15, y: 325), size: 50, kerning: 0)
+canvas.drawText(message: "no age limit", at: Point(x: 15, y: 395), size: 10, kerning: 0)
+canvas.drawText(message: "advance $6.50, door $7.50", at: Point(x: 15, y: 410), size: 10, kerning: 0)
+canvas.drawText(message: "friday,april 13 1984", at: Point(x: 15, y: 425), size: 10, kerning: 0)
+canvas.drawText(message: "los angles, california", at: Point(x: 150, y: 395), size: 10, kerning: 0)
+canvas.drawText(message: "1801 s. grand ave.", at: Point(x: 150, y: 410), size: 10, kerning: 0)
+canvas.drawText(message: "at olympic auditorium", at: Point(x: 150, y: 425), size: 10, kerning: 0)
+
+
+/*:
+ ## Use Source Control
+ 
+ As this is a practice task, there is no hard deadline. However, completing this practice task – an assessment for learning – prior to attempting an assessment of learning – is strongly recommended.
+ 
+ Be sure to commit your work multiple times as you make progress on completing this task.
+
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+
+
+
 
 /*:
  ## Use Source Control
